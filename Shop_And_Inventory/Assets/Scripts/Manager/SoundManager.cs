21efa077142cc3f62d29ed7aaff1e7e900cbb498
Imran1720 <skimran1720@gmail.com>
2025-05-31 21:53:58 +0530

    Implemented SoundManager

---
+++ Shop_And_Inventory/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+    [Header("Audio Sources")]
+    [SerializeField]
+    private AudioSource audioSource_BG;
+    [SerializeField]
+    private AudioSource audioSource_SFX;
+
+    [Header("Audio Clips")]
+    [SerializeField]
+    private SoundType[] soundClips;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        PlayBGM(Sounds.BGM);
+    }
+
+    private void PlayBGM(Sounds _bgm)
+    {
+        AudioClip clip = GetClip(_bgm);
+        if (clip != null)
+        {
+            audioSource_BG.clip = clip;
+            audioSource_BG.Play();
+        }
+    }
+
+    public void PlaySoundFX(Sounds _sfx)
+    {
+        AudioClip clip = GetClip(_sfx);
+        if (clip != null)
+        {
+            audioSource_SFX.PlayOneShot(clip);
+        }
+    }
+
+    private AudioClip GetClip(Sounds bgm)
+    {
+        foreach (SoundType type in soundClips)
+        {
+            if (type.soundType == bgm) return type.soundClip;
+        }
+        return null;
+    }
+}
+
+
+
+
